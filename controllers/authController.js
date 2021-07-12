1c111d8bfe96cf2332da6a7ca8983b7b66144479
ShoonLeiNaing <pandapanda254.pp@gmail.com>
2021-07-12 14:00:33 +0630

    email already existed feature done

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,11 +2,17 @@ import User from '../models/userModel.js'
 import {registerValidation} from '../utils/validation.js'
 import Joi from '@hapi/joi'
 
-export const register=(req,res)=>{
+export const register=async (req,res)=>{
     //validation 
     const {error} = registerValidation(req.body); 
     if(error){
         return res.status(400).send(error.details[0].message)
+    }
+
+    //checking email
+    const emailExist =await User.findOne({email:req.body.email})
+    if(emailExist){
+        return res.status(400).send("User already existed with that email")
     }
 
     //creating new user
